0ce3a1062fac89bec0edcaa5956598907dfbe78c
moha6248 <51365122+moha6248@users.noreply.github.com>
2021-05-05 12:23:45 +0200

    Add files via upload

---
+++ examProject21/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace examProject21.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
